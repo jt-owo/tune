7e6433ac1797fd37a86ca665a675fcee4575bba9
jt-owo <joniwalkerlp@gmail.com>
2020-04-28 06:28:20 +0200

    Add title bar

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,10 +1,21 @@
 import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
+import TitleBar from './TitleBar/TitleBar';
 import ExampleContainer from '../containers/ExampleContainer';
 
+require('./Application.scss');
+
+function LoadTitleBar(props: any) {
+    if (process.platform !== 'darwin') {
+        return <TitleBar />;
+    }
+    return <div />;
+}
+
 const Application = () => (
-    <div>
+    <div id="container">
+        <LoadTitleBar />
         <ExampleContainer message="tune." />
     </div>
 );
---
+++ src/renderer/components/TitleBar/TitleBar.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import * as React from 'react';
+
+require('./TitleBar.scss'); // Include Styles
+
+const electron = window.require('electron');
+const win = electron.remote.getCurrentWindow();
+
+class TitleBar extends React.Component {
+    minimize = () => {
+        win.minimize();
+    };
+
+    maximize = () => {
+        if (!win.isMaximized()) {
+            win.maximize();
+        } else {
+            win.unmaximize();
+        }
+    };
+
+    close = () => {
+        win.close();
+    };
+
+    render() {
+        return (
+            <nav id="titlebar">
+                <div id="titleShown">tune.</div>
+                <div id="buttons">
+                    <div id="minimize" role="button" onClick={this.minimize}>
+                        <span>_</span>
+                    </div>
+                    <div id="maximize" role="button" onClick={this.maximize}>
+                        <span>■</span>
+                    </div>
+                    <div id="close" role="button" onClick={this.close}>
+                        <span>&times;</span>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default TitleBar;

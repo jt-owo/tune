985670de3d46afdc54caba3b141709c7791cd73c
jt-owo <joniwalkerlp@gmail.com>
2020-04-28 07:00:00 +0200

    Add navigation

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,7 +1,9 @@
 import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import TitleBar from './TitleBar/TitleBar';
+import Navigation from './Navigation/Navigation';
 import ExampleContainer from '../containers/ExampleContainer';
 
 require('./Application.scss');
@@ -16,7 +18,11 @@ function LoadTitleBar(props: any) {
 const Application = () => (
     <div id="container">
         <LoadTitleBar />
-        <ExampleContainer message="tune." />
+        <BrowserRouter>
+            <div>
+                <Navigation />
+            </div>
+        </BrowserRouter>
     </div>
 );
 
---
+++ src/renderer/components/Navigation/Navigation.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+
+require('./Navigation.scss');
+
+const logo = require('../../../../resources/images/logo.png');
+
+const electron = window.require('electron');
+const win = electron.remote.getCurrentWindow();
+
+const Navigation: React.FunctionComponent<any> = () => (
+    <nav id="nav-bar-div">
+        <ul id="nav-bar">
+            <li>
+                <img src={logo} alt="logo" id="nav-bar-logo" draggable="false" />
+            </li>
+            <li>
+                <NavLink
+                    to="/home"
+                    className="nav-btn btn-hover-animation"
+                    id="home-btn"
+                    draggable="false">
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to="/browse"
+                    className="nav-btn btn-hover-animation"
+                    id="browse-btn"
+                    draggable="false">
+                    Browse
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to="/library"
+                    className="nav-btn btn-hover-animation"
+                    id="library-btn"
+                    draggable="false">
+                    Library
+                </NavLink>
+            </li>
+            <li id="pinned-playlist-section">PLAYLISTS</li>
+            <li>
+                <NavLink
+                    to="/settings"
+                    className="nav-btn btn-hover-animation"
+                    id="settings-btn"
+                    draggable="false">
+                    Settings
+                </NavLink>
+            </li>
+        </ul>
+    </nav>
+);
+
+export default Navigation;

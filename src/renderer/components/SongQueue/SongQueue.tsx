85d601705b297a3bd91da7a4d0aafc2ee14829f6
tobytaken <tobiassteinhaeusler@gmail.com>
2020-06-17 11:42:02 +0200

    Change img src to import

--- src/renderer/components/SongQueue/SongQueue.tsx
+++ src/renderer/components/SongQueue/SongQueue.tsx
@@ -2,28 +2,32 @@ import * as React from 'react';
 
 import './SongQueue.scss';
 
+import ac1 from '../../../../resources/test-images/album-cover-1.jpg';
+import ac2 from '../../../../resources/test-images/album-cover-2.jpg';
+import ac3 from '../../../../resources/test-images/album-cover-3.jpg';
+
 const SongQueue = () => {
     return (
         <div id="song-queue-container">
             <header>Up Next</header>
             <div id="song-queue">
                 <div>
-                    <img src="../../../../resources/test-images/album-cover-1.jpg" alt="" />
+                    <img src={ac1} alt="" />
                 </div>
                 <div>
-                    <img src="../../../../resources/test-images/album-cover-1.jpg" alt="" />
+                    <img src={ac1} alt="" />
                 </div>
                 <div>
-                    <img src="../../../../resources/test-images/album-cover-2.jpg" alt="" />
+                    <img src={ac2} alt="" />
                 </div>
                 <div>
-                    <img src="../../../../resources/test-images/album-cover-3.jpg" alt="" />
+                    <img src={ac3} alt="" />
                 </div>
                 <div>
-                    <img src="../../../../resources/test-images/album-cover-3.jpg" alt="" />
+                    <img src={ac3} alt="" />
                 </div>
                 <div>
-                    <img src="../../../../resources/test-images/album-cover-1.jpg" alt="" />
+                    <img src={ac1} alt="" />
                 </div>
             </div>
         </div>

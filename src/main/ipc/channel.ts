b79d7cecd81c9033125e0b3e81e9c0d0500612a1
jt-owo <jt-owo@outlook.com>
2022-12-24 18:38:18 +0100

    Add ipc shell url channel

--- src/main/ipc/channel.ts
+++ src/main/ipc/channel.ts
@@ -2,6 +2,7 @@ export enum Channels {
 	WINDOW_CONTROLS = 'window-controls',
 	SELECT_FILE = 'select-file',
 	READ_METADATA = 'read-metadata',
+	OPEN_URL = 'open-shell-external',
 	DATABASE_GET = 'db-get-value',
 	DATABASE_SET = 'db-set-value'
 }

06491a663f02c6ac41f08a2643637064f119fe12
tobytaken <tobiassteinhaeusler@gmail.com>
2022-12-27 17:26:59 +0100

    Add HomeItemSmall & Medium

--- src/ui/views/Home/Home.tsx
+++ src/ui/views/Home/Home.tsx
@@ -4,15 +4,20 @@
 import * as React from 'react';
 import View from '../../components/View/View';
 import ContextMenuExample from '../../components/ContextMenu/ContextMenuExample';
-import Dialog from '../../components/Dialog/Dialog';
+import HomeItemSmall from '../../components/Home_Elements/HomeItemSmall/HomeItemSmall';
+import HomeItemMedium from '../../components/Home_Elements/HomeItemMedium/HomeItemMedium';
 
 import './Home.scss';
+
+import image1 from '../../../../assets/images/tune_no_artwork.svg';
 
 const Home: React.FC = () => {
 	return (
 		<View title="Home" id="home">
 			<div className="content">
 				<ContextMenuExample />
+				<HomeItemSmall image={image1} title="Awesome Album" artist="TUNE" />
+				<HomeItemMedium image={image1} title="Awesome Album" artist="TUNE" />
 			</div>
 		</View>
 	);

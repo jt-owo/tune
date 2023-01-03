f74409b4e73db69b12f5ebe0f08e88e420428c17
jt-owo <jt-owo@outlook.com>
2023-01-04 00:28:16 +0100

    Add Portal to ToolTip

--- src/ui/components/ToolTip/ToolTip.tsx
+++ src/ui/components/ToolTip/ToolTip.tsx
@@ -1,18 +1,46 @@
 import * as React from 'react';
+import { FC, useState } from 'react';
+import useMousePosition from '../../hooks/useMousePosition';
+import Portal from '../Portal/Portal';
+
 import './ToolTip.scss';
 
 interface ToolTipProps {
+	/**
+	 * Text the tooltip should displays.
+	 */
 	text: string;
+	/**
+	 * Number of the y offset which should be subtracted. Default value is 70.
+	 */
+	offsetY?: number;
 	children: React.ReactNode | React.ReactNode[];
 }
 
-const ToolTip: React.FC<ToolTipProps> = (props) => {
-	const { text, children } = props;
+const ToolTip: FC<ToolTipProps> = (props) => {
+	const { text, children, offsetY } = props;
+	const [active, setActive] = useState(false);
+
+	const mousePosition = useMousePosition();
+
+	const mouseEnter = () => {
+		setActive(true);
+	};
+
+	const mouseLeave = () => {
+		setActive(false);
+	};
 
 	return (
-		<div className="tooltip">
+		<div className="tooltip-container" onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
 			{children}
-			<span className="tooltip-text">{text}</span>
+			{active && (
+				<Portal wrapperID="tooltip-wrapper">
+					<div style={{ left: mousePosition.x, top: mousePosition.y - (offsetY || 50) }} className="tooltip-text">
+						{text}
+					</div>
+				</Portal>
+			)}
 		</div>
 	);
 };
--- src/ui/views/Library/Library.tsx
+++ src/ui/views/Library/Library.tsx
@@ -24,27 +24,18 @@ const Library: React.FC = () => {
 	return (
 		<View title="Library" id="library">
 			<div className="content">
-				<ToolTip text="Show an alert message">
-					<button type="button" onClick={() => testAlert('This is a info message', 'info')}>
-						Info Alert
-					</button>
-				</ToolTip>
-				<ToolTip text="Show an success message">
-					<button type="button" onClick={() => testAlert('This is a success message', 'success')}>
-						Success Alert
-					</button>
-				</ToolTip>
-
-				<ToolTip text="Show an warning message">
-					<button type="button" onClick={() => testAlert('This is a warn message', 'warn')}>
-						Warn Alert
-					</button>
-				</ToolTip>
-				<ToolTip text="Show an error message">
-					<button type="button" onClick={() => testAlert('This is a error message', 'error')}>
-						Error Alert
-					</button>
-				</ToolTip>
+				<button type="button" onClick={() => testAlert('This is a info message', 'info')}>
+					Info Alert
+				</button>
+				<button type="button" onClick={() => testAlert('This is a success message', 'success')}>
+					Success Alert
+				</button>
+				<button type="button" onClick={() => testAlert('This is a warn message', 'warn')}>
+					Warn Alert
+				</button>
+				<button type="button" onClick={() => testAlert('This is a error message', 'error')}>
+					Error Alert
+				</button>
 			</div>
 		</View>
 	);

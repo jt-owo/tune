2276f99f7c742c1a54a62568a3951b730f9d8005
tobytaken <tobiassteinhaeusler@gmail.com>
2023-02-18 23:47:57 +0100

    Add TabControl to Settings, Update style

--- src/ui/views/Settings/Settings.tsx
+++ src/ui/views/Settings/Settings.tsx
@@ -9,6 +9,8 @@ import '../../styles/_components.scss';
 
 import spotifyLogo from '../../../../assets/service-icons/Spotify_Logo_RGB_White.png';
 import appleMusicIcon from '../../../../assets/service-icons/Apple_Music_Icon_W.svg';
+import TabControl from '../../components/TabControl/TabControl';
+import TabItem from '../../components/TabControl/TabItem/TabItem';
 
 const Settings: FC = () => {
 	const selectedOutputDevice = useAppSelector(selectOutputDeviceId);
@@ -33,22 +35,31 @@ const Settings: FC = () => {
 	return (
 		<View title="Settings" id="settings">
 			<div className="content">
-				<select className="dropdown-select-1" onChange={onOutputDeviceChange} value={selectedOutputDevice || 'default'}>
-					{audioDevices &&
-						audioDevices.map((device) => {
-							return (
-								<option key={device.deviceId} value={device.deviceId}>
-									{device.deviceId.toLowerCase() === 'default' ? 'Default Audio Device' : device.label}
-								</option>
-							);
-						})}
-				</select>
-				<a href="/" className="service-btn" id="spotify">
-					Connect to <img src={spotifyLogo} alt="" />
-				</a>
-				<a href="/" className="service-btn" id="apple-music">
-					Connect to <img src={appleMusicIcon} alt="" /> Apple Music
-				</a>
+				<TabControl>
+					<TabItem label="Audio Settings">
+						<select className="dropdown-select-1" onChange={onOutputDeviceChange} value={selectedOutputDevice || 'default'}>
+							{audioDevices &&
+								audioDevices.map((device) => {
+									return (
+										<option key={device.deviceId} value={device.deviceId}>
+											{device.deviceId.toLowerCase() === 'default' ? 'Default Audio Device' : device.label}
+										</option>
+									);
+								})}
+						</select>
+					</TabItem>
+					<TabItem label=" Streaming Services">
+						<a href="/" className="service-btn" id="spotify">
+							Connect to <img src={spotifyLogo} alt="" />
+						</a>
+						<a href="/" className="service-btn" id="apple-music">
+							Connect to <img src={appleMusicIcon} alt="" /> Apple Music
+						</a>
+					</TabItem>
+					<TabItem label="About">
+						<p>tune. 0.1.0</p>
+					</TabItem>
+				</TabControl>
 			</div>
 		</View>
 	);
